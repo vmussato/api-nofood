4f52a1c4e1905c72fb318dd4360b5f2eed549476
Vmussato <vinicius.mussato@gmail.com>
2018-09-05 20:56:00 -0300

    Added mongodb config

--- bin/express.js
+++ bin/express.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const variables = require('../bin/configuration/variables');
 
 //routers
 const categoriaRouter = require('../routes/categoria-router');
@@ -12,6 +14,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+//Configurando a conexão com o banco de dados
+mongoose.connect(variables.Database.connection);
 
 //Configuração das rotas
 app.use('/api/categoria', categoriaRouter);
