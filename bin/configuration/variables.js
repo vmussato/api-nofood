50f78209bad52fe3e033437b90a9c7bf2bb8355b
Vmussato <vinicius.mussato@gmail.com>
2018-09-05 21:48:17 -0300

    created models and controller for categoria

--- bin/configuration/variables.js
+++ bin/configuration/variables.js
@@ -1,7 +1,7 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
-    }
+    },
     Database: {
         connection: process.env.connection || 'mongodb://admin:nofood123456@ds245772.mlab.com:45772/nofood'
     }
